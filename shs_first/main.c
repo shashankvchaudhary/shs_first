be798b1dc8af466528644713e22ce54c90d090b1
Shashank Chaudhary <shashankchaudhary@Shashanks-MacBook-Pro.local>
2014-01-05 22:17:54 +0530

    Initial Commit

---
+++ shs_first/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  shs_first
+//
+//  Created by Shashank Chaudhary on 05/01/14.
+//  Copyright (c) 2014 Shashank Chaudhary. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
